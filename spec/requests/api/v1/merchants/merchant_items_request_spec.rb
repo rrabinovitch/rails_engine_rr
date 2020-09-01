d9c55bf689249f4c0d0411923b2b76c36e930d43
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-31 21:21:06 -0600

    Test merchant items endpoint

---
+++ spec/requests/api/v1/merchants/merchant_items_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Items endpoint' do
+  it 'returns all items associated with a merchant' do
+    merchant = create(:merchant)
+    3.times {create(:item, merchant: merchant)}
+    diff_merchant_item = create(:item)
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+
+    merchant_items_json = JSON.parse(response.body, symbolize_names: true)
+    expect(merchant_items_json[:data].count).to eq(3)
+    expect(merchant_items_json[:data].first).to have_key(:id)
+    # expect(merchant_items_json[:data]) to not have item w id of last item
+  end
+end
