abe408333aa2ca9e55a449a648bb63eddb09a6b2
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-31 22:45:42 -0600

    Create Items/Merchant#index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, except: [:new, :edit]
+      resources :items, except: [:new, :edit] do
+        resources :merchant, only: [:index], module: :items
+        # get '/merchant', to: 'merchant#show', module: :items
+      end
+
       resources :merchants, except: [:new, :edit] do
         resources :items, only: [:index], module: :merchants
       end
