f2f2937a5c920940393e852230d760177cfe471f
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-01 17:51:16 -0600

    Update ItemsController strong params and tests to pass spec harness

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -22,6 +22,6 @@ class Api::V1::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 end
