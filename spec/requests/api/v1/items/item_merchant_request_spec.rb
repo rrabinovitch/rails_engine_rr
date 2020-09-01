a407fa24228031df7ff88d39512fa652d757b0d3
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-31 22:46:28 -0600

    Add more robust expectations

--- spec/requests/api/v1/items/item_merchant_request_spec.rb
+++ spec/requests/api/v1/items/item_merchant_request_spec.rb
@@ -10,7 +10,9 @@ RSpec.describe 'Item Merchant endpoint' do
     expect(response.content_type).to eq("application/json")
 
     item_merchant_json = JSON.parse(response.body, symbolize_names: true)
-    expect(item_merchant_json[:data].count).to eq(1)
-    # expect(item_merchant_json[:data])
+    expect(item_merchant_json[:data][:id]).to eq(merchant.id.to_s)
+    expect(item_merchant_json[:data][:type]).to eq("merchant")
+    expect(item_merchant_json[:data][:attributes][:name]).to eq(merchant.name)
+    expect(item_merchant_json[:data][:relationships][:items][:data].first[:id]).to eq(item.id.to_s)
   end
 end
