087748530d8f743292d394740b13564bfd22e4f1
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-31 19:08:22 -0600

    Remove id attribute from ItemSerializer; add more expectations to Items#index spec

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :unit_price
+  attributes :name, :description, :unit_price
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -13,6 +13,9 @@ describe 'Items endpoints' do
     expect(items[:data].first).to have_key(:id)
     expect(items[:data].first).to have_key(:type)
     expect(items[:data].first).to have_key(:attributes)
+    expect(items[:data].first[:attributes]).to have_key(:name)
+    expect(items[:data].first[:attributes]).to have_key(:description)
+    expect(items[:data].first[:attributes]).to have_key(:unit_price)
   end
 
   it 'Items#show' do
