bb65e704005f19bcd3784be0e906c9a5023c5c3b
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-31 18:00:09 -0600

    Write test for Items#update endpoint

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     name { "A fancy item" }
-  description { "This is a *~very fancy~* item." }
+    description { "This is a *~very fancy~* item." }
     unit_price { 5.5 }
     merchant
   end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -25,11 +25,26 @@ describe 'Items endpoints' do
     merchant = create(:merchant)
     item_params = { name: "Cheap item", description: "This is just a cheap item.", unit_price: 2.75, merchant_id: merchant.id }
     headers = { "CONTENT_TYPE" => "application/json" }
-    post "/api/v1/items", params: JSON.generate({item: item_params}), headers: headers
 
+    post "/api/v1/items", params: JSON.generate({item: item_params}), headers: headers
     expect(response).to be_successful
 
     item = Item.last
+    expect(item.name).to eq(item_params[:name])
+  end
+
+  it 'Items#update' do
+    item_id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "New Item Name" }
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    put "/api/v1/items", params: JSON.generate({item: item_params}), headers: headers
+    expect(response).to be_successful
+
+    item = Item.find_by(id: item_id)
+
+    expect(item.name).to_not eq(previous_name)
     expect(item.name).to eq(item_params[:name])
   end
 end
