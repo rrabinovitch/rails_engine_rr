e4ec664cb3792d6c16339746a37b0d951ccf73d5
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 14:33:12 -0600

    Refactoring notes for later

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -3,10 +3,12 @@ class Api::V1::Items::SearchController < ApplicationController
     attribute = request.query_parameters.keys.first
     search_value = request.query_parameters.values.first
     # if there's more than one query param or if the first attribute is not one of our model attributes, send an error
+    # refactor into Item.find_by_attribute(attribute, search_value) class method in model file
+      # model method can identify whether to search for id/unit_price or other attributes
     if params[:id] || params[:unit_price]
       item = Item.find_by("#{attribute}": search_value)
     else
-      item = Item.where("#{attribute} ILIKE '%#{search_value}%'").first
+      item = Item.find_by("#{attribute} ILIKE '%#{search_value}%'")
     end
     render json: ItemSerializer.new(item)
   end
