d26c445ceaf0069230c8b92f3a8b9d4483277994
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 16:06:57 -0600

    Create #index action for multi-finder and refactor out private methods for search attribute and value

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,18 +1,33 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    attribute = request.query_parameters.keys.first
-    search_value = request.query_parameters.values.first
     # if there's more than one query param or if the first attribute is not one of our model attributes, send an error
     # refactor into Item.find_by_attribute(attribute, search_value) class method in model file
       # model method can identify whether to search for id/unit_price or other attributes
+      # OR refactor into filter_by method that can be used for both single and multi-finders
+        # and just call .first on the return value for #show action
     if params[:id] || params[:unit_price]
       item = Item.find_by("#{attribute}": search_value)
     # elsif params[:created_at] || params[:updated_at]
-    #   binding.pry
+    #   LOGIC FOR FINDING ITEM BY CREATED_AT AND UPDATED_AT ATTRIBUTES
     else
       item = Item.find_by("#{attribute} ILIKE '%#{search_value}%'")
     end
     render json: ItemSerializer.new(item)
+  end
+
+  def index
+    items = Item.where("#{attribute} ILIKE '%#{search_value}'")
+    render json: ItemSerializer.new(items)
+  end
+
+  private
+
+  def attribute
+    request.query_parameters.keys.first
+  end
+
+  def search_value
+    request.query_parameters.values.first
   end
 end
 
