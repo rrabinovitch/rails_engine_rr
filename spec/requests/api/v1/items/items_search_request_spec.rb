6319708def610ddf02e23fb4dd1d8c9ee67207b3
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 10:13:37 -0600

    Outline specs for items single finders endpoints

---
+++ spec/requests/api/v1/items/items_search_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Items search endpoints' do
+  describe 'Single finders' do
+    it 'Can find a single item by its full name' do
+
+    end
+
+    it 'Can find a single item by a fragment of its name' do
+
+    end
+
+    it 'Can find a single item by description' do
+
+    end
+
+    it 'Can find a single item by price' do
+
+    end
+
+    it 'Can find a single item by the date it was created' do
+
+    end
+
+    it 'Can find a single item by the date it was updated' do
+
+    end
+  end
+end
