de3309b1b6cdd7c84d2bf7eb196cd4a61ba7ee89
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 16:18:42 -0600

    Test for finding multiple items by description fragment

--- spec/requests/api/v1/items/items_search_request_spec.rb
+++ spec/requests/api/v1/items/items_search_request_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Items search endpoints' do
   before :each do
     create_list(:item, 4, created_at: "2012-03-27 14:53:59", updated_at: "2012-04-15 14:00:05")
     @found_item = Item.first
+    @not_found_item = create(:item, name: "Don't Find Me", description: "I shouldn't be found.")
   end
 
   describe 'Single finders' do
@@ -50,8 +51,8 @@ RSpec.describe 'Items search endpoints' do
       expect(search_results_json[:data][:attributes][:merchant_id]).to eq(@found_item.merchant_id)
     end
 
-    it 'Can find a single item by description fragment' do
-      search_value = @found_item.description[12..21]
+    it 'Can find a single item by description fragment, insensitive to case' do
+      search_value = @found_item.description[12..21].upcase
       get "/api/v1/items/find?description=#{search_value}"
       expect(response).to be_successful
       expect(response.content_type).to eq("application/json")
@@ -109,7 +110,7 @@ RSpec.describe 'Items search endpoints' do
   end
 
   describe 'Multi-finders' do
-    it 'Can find set of items by full name, insensitive to case' do
+    it 'Can find multiple items by full name, insensitive to case' do
       get "/api/v1/items/find_all?name=#{@found_item.name.upcase}"
       expect(response).to be_successful
       expect(response.content_type).to eq("application/json")
@@ -123,9 +124,10 @@ RSpec.describe 'Items search endpoints' do
       expect(search_results_json[:data].first[:attributes]).to have_key(:description)
       expect(search_results_json[:data].first[:attributes]).to have_key(:unit_price)
       expect(search_results_json[:data].first[:attributes]).to have_key(:merchant_id)
+      # add expectation to not have not_found_item
     end
 
-    it 'Can find set of items by name fragment, insensitive to case' do
+    it 'Can find multiple items by name fragment, insensitive to case' do
       search_value = @found_item.name[2..6].upcase
       get "/api/v1/items/find_all?name=#{search_value}"
       expect(response).to be_successful
@@ -140,6 +142,25 @@ RSpec.describe 'Items search endpoints' do
       expect(search_results_json[:data].first[:attributes]).to have_key(:description)
       expect(search_results_json[:data].first[:attributes]).to have_key(:unit_price)
       expect(search_results_json[:data].first[:attributes]).to have_key(:merchant_id)
+      # add expectation to not have not_found_item
+    end
+
+    it 'Can find multiple items by description fragment, insensitive to case' do
+      search_value = @found_item.description[12..21].upcase
+      get "/api/v1/items/find_all?description=#{search_value}"
+      expect(response).to be_successful
+      expect(response.content_type).to eq("application/json")
+
+      search_results_json = JSON.parse(response.body, symbolize_names: true)
+      expect(search_results_json[:data].count).to eq(4)
+      expect(search_results_json[:data].first).to have_key(:id)
+      expect(search_results_json[:data].first[:type]).to eq("item")
+      expect(search_results_json[:data].first).to have_key(:attributes)
+      expect(search_results_json[:data].first[:attributes]).to have_key(:name)
+      expect(search_results_json[:data].first[:attributes]).to have_key(:description)
+      expect(search_results_json[:data].first[:attributes]).to have_key(:unit_price)
+      expect(search_results_json[:data].first[:attributes]).to have_key(:merchant_id)
+      # add expectation to not have not_found_item
     end
   end
 end
