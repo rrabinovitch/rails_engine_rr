20fa0e12fab41fbdac40841b95c85b60c28f31c0
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 13:09:44 -0600

    Update Items/SearchController#show to take any full or partial attribute and search value

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,10 +1,20 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    if params[:id]
-      item = Item.find(params[:id])
-    elsif params[:name]
-      item = Item.find_by(name: params[:name])
-    end
+    attribute = request.query_parameters.keys.first
+    search_value = request.query_parameters.values.first
+    item = Item.where("#{attribute} LIKE '%#{search_value}%'").first
+    # if params[:id]
+    #   item = Item.find(params[:id])
+    # elsif params[:name]
+    #   item = Item.find_by(name: params[:name])
+    # end
     render json: ItemSerializer.new(item)
   end
 end
+
+
+# attribute = request.query_parameters.keys.first
+  # => "name"
+# search_value = request.query_parameters.values.first
+  # => "Fin"
+# Item.where("name LIKE '%Fin%'")
