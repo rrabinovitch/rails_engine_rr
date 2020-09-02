3c05121379a39aca77a1938b79fcfb021dbb8d6c
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 12:16:41 -0600

    Update Items/SearchController#show to take name search attribute

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    item = Item.find(params[:id])
+    if params[:id]
+      item = Item.find(params[:id])
+    elsif params[:name]
+      item = Item.find_by(name: params[:name])
+    end
     render json: ItemSerializer.new(item)
   end
 end
