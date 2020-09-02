4d7b3355523f4820206f4e16fce55e16eb7f2647
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 11:56:57 -0600

    Create items single finder route

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, except: [:new, :edit]
 
       namespace :items do
+        get '/find', to: 'search#show'
         get '/:id/merchant', to: 'merchant#show'
       end
 
       resources :merchants, except: [:new, :edit] do
         resources :items, only: [:index], module: :merchants
       end
+
+      resources :items, except: [:new, :edit]
     end
   end
 end
--- spec/requests/api/v1/items/items_search_request_spec.rb
+++ spec/requests/api/v1/items/items_search_request_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe 'Items search endpoints' do
   describe 'Single finders' do
     before :each do
       create_list(:item, 4)
-      found_item_1 = Item.first
-      found_item_2 = create(:item, name: "Find Me")
+      @found_item_1 = Item.first
+      @found_item_2 = create(:item, name: "Find Me")
     end
 
     it 'Can find a single item by its id' do
-      get "/api/v1/items/find?id=#{found_item_1.id}"
+      get "/api/v1/items/find?id=#{@found_item_1.id}"
       expect(response).to be_successful
       expect(response.content_type).to eq("application/json")
 
       search_results_json = JSON.parse(response.body, symbolize_names: true)
-      expect(search_results_json[:data][:id]).to eq(found_item_1.id.to_s)
+      expect(search_results_json[:data][:id]).to eq(@found_item_1.id.to_s)
       expect(search_results_json[:data][:type]).to eq("item")
       expect(search_results_json[:data]).to have_key(:attributes)
       expect(search_results_json[:data][:attributes]).to have_key(:name)
