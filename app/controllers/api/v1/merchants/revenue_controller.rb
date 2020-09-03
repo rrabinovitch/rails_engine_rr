e3673f969bb27c198c1709fd94b36aa0c06828d2
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-03 01:18:36 -0600

    Reorganize #index

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
+    merchants = Merchant.most_revenue(params[:quantity])
+    render json: MerchantSerializer.new(merchants)
   end
 end
