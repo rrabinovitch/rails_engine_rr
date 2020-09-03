4fdb91022dd22af6db43573dfa5c527aa85716d9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 23:09:38 -0600

    Edit factories

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :customer do
-    first_name { "MyString" }
-    last_name { "MyString" }
+    first_name { "Ruthie" }
+    last_name { "Rabinovitch" }
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :transaction do
-    invoice { "" }
-    credit_card_number { "MyString" }
+    invoice
+    credit_card_number { "123456789098765" }
     credit_card_expiration_date { "2020-08-30" }
-    result { 1 }
+    result { "success" }
   end
 end
