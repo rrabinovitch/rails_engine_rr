a45b37dab6220d539ffb1928791d95c03c12175f
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 20:16:45 -0600

    Create merchants mulit finder route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
       namespace :merchants do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
 
       resources :merchants, except: [:new, :edit] do
