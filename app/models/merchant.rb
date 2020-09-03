3cd2fc064e2c5e60a05c2c2084fec1ef937537f4
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-03 01:48:12 -0600

    Fix ::most_revenue to pass spec harness)

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,6 +7,11 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def self.most_revenue(quantity)
-    Merchant.joins(:transactions, :invoice_items).select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue").where("transactions.result='success' AND invoices.status='shipped'").group(:id).order("revenue desc").limit(quantity)
+    Merchant.joins(invoices: :items).joins(invoices: :transactions)
+      .select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+      .where("transactions.result='success' AND invoices.status='shipped'")
+      .group('merchants.id')
+      .order('revenue DESC')
+      .limit(quantity)
   end
 end
