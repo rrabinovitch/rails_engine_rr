86f766b740cde6ab71fe2e1ef2505d0bdcbff4d6
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 20:06:03 -0600

    Define #show method and helper methods for attribute and search_value

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
+    if params[:id]
+      item = Merchant.find_by("#{attribute}": search_value)
+    # elsif params[:created_at] || params[:updated_at]
+    #   LOGIC FOR FINDING ITEM BY CREATED_AT AND UPDATED_AT ATTRIBUTES
+    else
+      item = Merchant.find_by("#{attribute} ILIKE '%#{search_value}%'")
+    end
+    render json: MerchantSerializer.new(item)
+  end
 
+  private
+
+  def attribute
+    request.query_parameters.keys.first
+  end
+
+  def search_value
+    request.query_parameters.values.first
   end
 end
