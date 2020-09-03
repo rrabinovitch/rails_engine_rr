d3300e3ef535f26908980af84cc78aff7ef196dc
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 20:39:46 -0600

    Clean up

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    # if there's more than one query param or if the first attribute is not one of our model attributes, send an error
-    # refactor into Item.find_by_attribute(attribute, search_value) class method in model file
-      # model method can identify whether to search for id/unit_price or other attributes
-      # OR refactor into filter_by method that can be used for both single and multi-finders
-        # and just call .first on the return value for #show action
     if params[:id] || params[:unit_price]
       item = Item.find_by("#{attribute}": search_value)
     # elsif params[:created_at] || params[:updated_at]
@@ -34,6 +29,3 @@ class Api::V1::Items::SearchController < ApplicationController
     request.query_parameters.values.first
   end
 end
-
-# dont worry abotu partial matches for ids and prices
-# dates can be changed to string
