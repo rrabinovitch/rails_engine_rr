2f32d9910bb2509d0b849e8c226838fb4ed77f3d
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 23:31:04 -0600

    Remove cc exp date from transactions factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :transaction do
     invoice
     credit_card_number { "123456789098765" }
-    credit_card_expiration_date { "2020-08-30" }
     result { "success" }
   end
 end
