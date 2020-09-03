d9b32b6a0683936015844e7904e1ee81bd0695a9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-02 20:15:52 -0600

    Test additional single finder attribute searches; test multi finder search by full name

--- spec/requests/api/v1/merchants/merchants_serach_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_serach_request_spec.rb
@@ -29,5 +29,43 @@ RSpec.describe 'Merchants search endpoints' do
       expect(search_results_json[:data][:type]).to eq("merchant")
       expect(search_results_json[:data][:attributes][:name]).to eq(@found_merchant.name)
     end
+
+    it 'Can find a single merchant by a fragment of its name, insensitive to case' do
+      search_value = @found_merchant.name[0..2].upcase
+      get "/api/v1/merchants/find?name=#{search_value}"
+      expect(response).to be_successful
+      expect(response.content_type).to eq("application/json")
+
+      search_results_json = JSON.parse(response.body, symbolize_names: true)
+      expect(search_results_json[:data][:id]).to eq(@found_merchant.id.to_s)
+      expect(search_results_json[:data][:type]).to eq("merchant")
+      expect(search_results_json[:data][:attributes][:name]).to eq(@found_merchant.name)
+    end
+
+    xit 'Can find a single merchant by the date it was created' do
+      get "/api/v1/merchants/find?created_at=#{@found_merchant.created_at}"
+      expect(response).to be_successful
+      expect(response.content_type).to eq("application/json")
+
+      search_results_json = JSON.parse(response.body, symbolize_names: true)
+      expect(search_results_json[:data][:id]).to eq(@found_merchant.id.to_s)
+      expect(search_results_json[:data][:type]).to eq("merchant")
+      expect(search_results_json[:data][:attributes][:name]).to eq(@found_merchant.name)
+    end
+  end
+
+  describe 'Multi-finders' do
+    it 'Can find multiple merchants by full name, insensitive to case' do
+      get "/api/v1/merchants/find_all?name=#{@found_merchant.name.upcase}"
+      expect(response).to be_successful
+      expect(response.content_type).to eq("application/json")
+
+      search_results_json = JSON.parse(response.body, symbolize_names: true)
+      expect(search_results_json[:data].count).to eq(4)
+      expect(search_results_json[:data].first).to have_key(:id)
+      expect(search_results_json[:data].first[:type]).to eq("merchant")
+      expect(search_results_json[:data].first).to have_key(:attributes)
+      expect(search_results_json[:data].first[:attributes]).to have_key(:name)
+    end
   end
 end
